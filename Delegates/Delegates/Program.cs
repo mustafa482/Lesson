a29e11a862b95a2fb00e54d4010edcb69a46bab9
Mustafa <mustafa.mustafayev482@gmail.com>
2022-06-03 16:40:15 +0400

    delegate

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class Program
+    {
+       public delegate int Calculator(int n);
+        static void Main(string[] args)
+        {
+            Calculator c1 = DelegateExample.add;
+            Calculator c2 = DelegateExample.mul;
+            c1(20);
+            Console.WriteLine("After c1 delegate, Number is: " + DelegateExample.getNumber());
+            c2(3);
+            Console.WriteLine("After c2 delegate, Number is: " + DelegateExample.getNumber());
+
+
+        }
+    }
+    public class DelegateExample
+    {
+        static int number = 100;
+        public static int add(int n)
+        {
+            number = number + n;
+            return number;
+        }
+        public static int mul(int n)
+        {
+            number = number * n;
+            return number;
+        }
+        public static int getNumber()
+        {
+            return number;
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneriClass<string> gen = new GeneriClass<string>("This is generic class");
+            GeneriClass<int> genI = new GeneriClass<int>(101);
+            GeneriClass<char> gencCh = new GeneriClass<char>('G');
+
+            Generic generic = new Generic();
+            generic.Show("This is generic method");
+            generic.Show(100);
+            generic.Show('B');
+        }
+    }
+    public class GeneriClass<T>
+    {
+        public GeneriClass( T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    public class Generic
+    {
+        public void Show<B>(B message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
